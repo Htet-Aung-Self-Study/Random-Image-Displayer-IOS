20805ec39b1f25e3382f8019fb6d1e49d588703c
DreamerChaserHAH <109950820+DreamerChaserHAH@users.noreply.github.com>
2023-03-31 20:49:31 +0800

    Initial Commit

---
+++ random_image_displayer/random_image_displayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  random_image_displayerApp.swift
+//  random_image_displayer
+//
+//  Created by User on 31/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct random_image_displayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
